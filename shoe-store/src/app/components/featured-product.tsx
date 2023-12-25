e73d1d64f49e0a88e72c9b1f410d254c8214da40
Davi Patricio <davipatricio2006@gmail.com>
2023-12-25 14:46:48 -0300

    feat: add hero section

--- shoe-store/src/app/components/featured-product.tsx
+++ shoe-store/src/app/components/featured-product.tsx
@@ -1,5 +1,39 @@
-export default function FeaturedProduct() {
-  return <div className="w-full bg-gray-200 h-96">
+import Image from 'next/image';
+import FeaturedProductImage from '@/app/assets/products/featured-product.png';
+import { DM_Sans } from 'next/font/google';
+import clsx from 'clsx';
+import Button from './button';
+import { HiOutlineArrowRight } from 'react-icons/hi';
+import Link from 'next/link';
 
-  </div>
+const dmSans = DM_Sans({
+  display: 'swap',
+  weight: ['700'],
+  subsets: ['latin'],
+});
+
+export default function FeaturedProduct() {
+  return (
+    <div className="flex flex-col items-center gap-5 w-full bg-gray-200 py-10 px-5 text-center rounded-2xl">
+      <Image src={FeaturedProductImage} alt="Featured product" />
+
+      <div className={clsx('flex flex-col font-bold', dmSans.className)}>
+        <p className="text-4xl text-orange-600">25% OFF</p>
+        <p className="text-5xl">Summer Sale</p>
+      </div>
+
+      <div>
+        <p className="text-gray-700">
+          Discover our summer styles with discount
+        </p>
+      </div>
+
+      <Link href="/#drops" className="w-full">
+        <Button>
+          <p>Shop now</p>
+          <HiOutlineArrowRight className="text-xl" />
+        </Button>
+      </Link>
+    </div>
+  );
 }
