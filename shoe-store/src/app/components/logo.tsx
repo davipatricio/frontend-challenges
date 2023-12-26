88c09a69a247dee79245076c723c2fc6dc9f31e4
Davi Patricio <davipatricio2006@gmail.com>
2023-12-26 11:12:16 -0300

    fix logo variant

--- shoe-store/src/app/components/logo.tsx
+++ shoe-store/src/app/components/logo.tsx
@@ -11,7 +11,7 @@ export default function Logo({ variant = 'white' }: LogoProps) {
   return (
     <Link className="flex items-center gap-2 hover:text-zinc-700" href="/">
       <Image src={LogoImage} alt="Website logo" className="flex-shrink-0 size-7" />
-      <span className={clsx('uppercase font-semibold text-lg text-white', variant === 'black' && 'text-black')}>Sun Co.</span>
+      <span className={clsx('uppercase font-semibold text-lg text-white', variant === 'black' && '!text-black')}>Sun Co.</span>
     </Link>
   );
 }
