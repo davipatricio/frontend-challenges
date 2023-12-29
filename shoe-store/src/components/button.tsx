81c2783badb41704f116a8d895a45c1d4cc3d541
Davi Patricio <davipatricio2006@gmail.com>
2023-12-29 13:26:24 -0300

    improve disabled button styles

--- shoe-store/src/components/button.tsx
+++ shoe-store/src/components/button.tsx
@@ -2,7 +2,7 @@
 
 import clsx from 'clsx';
 
-type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
 
 export default function Button(props: ButtonProps) {
   return (
@@ -11,6 +11,7 @@ export default function Button(props: ButtonProps) {
       type="button"
       className={clsx(
         'bg-zinc-900 flex gap-3 items-center justify-center py-4 px-4 text-white w-full rounded-xl font-bold',
+        'disabled:bg-zinc-900/80 disabled:cursor-not-allowed',
         props.className,
       )}
     >
